33f97c3e86ef4a40d59b98faefb77ecfa55d5b81
xxyukixx <usagi.0325@gmail.com>
2021-06-15 23:28:50 +0900

    プロジェクト追加
    プロジェクト追加するよ

---
+++ DailyNotes/DailyNotes/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,31 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Reactive.Bindings;
+
+namespace DailyNotes.ViewModels
+{
+    public class MainPageViewModel : ViewModelBase
+    {
+        /// <summary>
+        /// 設定画面を開くコマンド
+        /// </summary>
+        AsyncReactiveCommand ShowSettingView { get; } = new AsyncReactiveCommand();
+
+        public MainPageViewModel(INavigationService navigationService)
+            : base(navigationService)
+        {
+            // タイトルの設定
+            Title = "メイン画面";
+
+            ShowSettingView.Subscribe(async _ =>
+            {
+                await NavigationService.NavigateAsync("MainPage/SettingPageView");
+            });
+        }
+    }
+}
---
+++ DailyNotes/DailyNotes/Views/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿
+namespace DailyNotes.Views
+{
+    public partial class MainPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
